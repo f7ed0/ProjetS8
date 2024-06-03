88103507948cd7561269bde181d76172514d0bbe
VerselT <Verseltristan@gmail.com>
2024-06-03 22:36:50 +0200

    chats

--- projetS8/src/app/main/main.component.ts
+++ projetS8/src/app/main/main.component.ts
@@ -1,14 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild, AfterViewInit, ChangeDetectionStrategy } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
-import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatSidenavModule, MatDrawer} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
+import {MatTooltipModule} from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [MatButtonModule, MatSidenavModule],
+  imports: [MatButtonModule, MatSidenavModule, MatIconModule, MatTooltipModule],
   templateUrl: './main.component.html',
-  styleUrl: './main.component.scss'
+  styleUrl: './main.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class MainComponent {
+export class MainComponent implements AfterViewInit{
+  @ViewChild('drawer') drawer!: MatDrawer;
+  isChevronLeft: boolean = false;
 
+  ngAfterViewInit() {
+    this.drawer.open();
+  }
+
+  toggleDrawer() {
+    this.drawer.toggle();
+    this.isChevronLeft = !this.drawer.opened;
+  }
+
+  toggleIcon() {
+    this.isChevronLeft = !this.isChevronLeft;
+  }
 }
