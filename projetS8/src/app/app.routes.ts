a2aa64da9ec8ac9db5a680434515df604a9a8311
VerselT <Verseltristan@gmail.com>
2024-06-03 17:04:16 +0200

    test debut

--- projetS8/src/app/app.routes.ts
+++ projetS8/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { NotFoundComponentComponent } from './not-found-component/not-found-component.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path :"**", component : NotFoundComponentComponent }
+];
