5b45c7bf04d196579869482bf36432ae9fc29b8f
VerselT <Verseltristan@gmail.com>
2024-06-03 17:11:57 +0200

    suppression

--- projetS8/src/app/app.component.ts
+++ projetS8/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NotFoundComponentComponent } from './not-found-component/not-found-component.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NotFoundComponentComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- projetS8/src/app/app.routes.ts
+++ projetS8/src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
-import { NotFoundComponentComponent } from './not-found-component/not-found-component.component';
 
 export const routes: Routes = [
-    {path :"**", component : NotFoundComponentComponent }
+    {path :"**" }
 ];
