e633c269ca65e0dcbe6211c2083d0175e189cd73
VerselT <Verseltristan@gmail.com>
2024-06-04 15:32:25 +0200

    messages

--- projetS8/src/app/messages/messages.component.ts
+++ projetS8/src/app/messages/messages.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {FormsModule} from '@angular/forms';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-messages',
   standalone: true,
-  imports: [MatInputModule, MatFormFieldModule, FormsModule],
+  imports: [MatInputModule, MatFormFieldModule, FormsModule, MatIconModule],
   templateUrl: './messages.component.html',
   styleUrl: './messages.component.scss'
 })
