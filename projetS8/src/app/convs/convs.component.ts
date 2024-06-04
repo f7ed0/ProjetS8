7323cacee2c2f1f93b64aa33d367c8d6df6b0c51
ahmedo <ahmed__daoud@outlook.fr>
2024-06-05 01:59:41 +0200

    Amélioration convs et ajout redirection

--- projetS8/src/app/convs/convs.component.ts
+++ projetS8/src/app/convs/convs.component.ts
@@ -1,12 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiServiceService } from '../api-service.service';
+import { CommonModule } from '@angular/common';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Router } from '@angular/router';
 
 @Component({
   selector: 'app-convs',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatIconModule,
+    HttpClientModule,
+    RouterModule
+  ],
   templateUrl: './convs.component.html',
-  styleUrl: './convs.component.scss'
+  styleUrls: ['./convs.component.scss'],
+  providers: [ApiServiceService]
 })
-export class ConvsComponent {
+export class ConvsComponent implements OnInit {
+  convs: any = [];
 
+  constructor(private apiService: ApiServiceService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.getDataDistinct();
+  }
+
+  getDataDistinct(): void {
+    this.apiService.getDataDistinct().subscribe({
+      next: (data: any) => {
+        console.log('Data received from API:', data); // Log the received data
+        this.convs = data;
+      },
+      error: (error) => {
+        console.error('There was an error!', error);
+        if (error.status === 0) {
+          console.error('Network error - make sure the API server is running.');
+        } else {
+          console.error(`Backend returned code ${error.status}, body was: ${error.error}`);
+        }
+      },
+      complete: () => {
+        console.log('Request completed');
+      }
+    });
+  }
+
+  navigateToChat(chatId: string): void {
+    this.router.navigate([`/chat/${chatId}`]);
+  }
 }
