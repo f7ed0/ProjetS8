87f17ddf3f6b508e9e9a471f13401924eddf5799
VerselT <Verseltristan@gmail.com>
2024-06-04 11:59:20 +0200

    header

--- projetS8/src/app/header/header.component.ts
+++ projetS8/src/app/header/header.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
+import {MatDialog, MatDialogModule} from '@angular/material/dialog';
+import { LoginComponent } from '../login/login.component';
 
 @Component({
   selector: 'app-header',
@@ -9,5 +11,17 @@ import {MatButtonModule} from '@angular/material/button';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+
+  constructor(
+    private dialog: MatDialog
+  ){}
+
+
+  openDialog() {
+    let d = this.dialog.open(LoginComponent, {data : undefined});
+    d.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`)
+    });
+  }
 
 }
