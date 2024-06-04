a8b1319671643242416f34016e66dfdc83d3dbdb
VerselT <Verseltristan@gmail.com>
2024-06-04 23:23:29 +0200

    suggestions

---
+++ projetS8/src/app/sugg/sugg.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sugg',
+  standalone: true,
+  imports: [],
+  templateUrl: './sugg.component.html',
+  styleUrl: './sugg.component.scss'
+})
+export class SuggComponent {
+
+}
