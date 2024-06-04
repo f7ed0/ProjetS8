15cd0b33a229df5d3fdaed6d7c6feef8c0fae2dc
VerselT <Verseltristan@gmail.com>
2024-06-04 11:59:49 +0200

    main

--- projetS8/src/app/main/main.component.ts
+++ projetS8/src/app/main/main.component.ts
@@ -3,18 +3,20 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatSidenavModule, MatDrawer} from '@angular/material/sidenav';
 import {MatIconModule} from '@angular/material/icon';
 import {MatTooltipModule} from '@angular/material/tooltip';
+import { MessagesComponent } from '../messages/messages.component';
+
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [MatButtonModule, MatSidenavModule, MatIconModule, MatTooltipModule],
+  imports: [MatButtonModule, MatSidenavModule, MatIconModule, MatTooltipModule, MessagesComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MainComponent implements AfterViewInit{
   @ViewChild('drawer') drawer!: MatDrawer;
-  isChevronLeft: boolean = false;
+  isChevronLeft: boolean = true;
 
   ngAfterViewInit() {
     this.drawer.open();
@@ -22,10 +24,13 @@ export class MainComponent implements AfterViewInit{
 
   toggleDrawer() {
     this.drawer.toggle();
-    this.isChevronLeft = !this.drawer.opened;
+    this.toggleIcon();
   }
 
   toggleIcon() {
     this.isChevronLeft = !this.isChevronLeft;
   }
+
 }
+
+
