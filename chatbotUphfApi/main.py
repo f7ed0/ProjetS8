b26fc41f956852108c0436e1c5289d2d4f1ef2ab
ahmedo <ahmed__daoud@outlook.fr>
2024-06-05 01:56:50 +0200

    Correction bug CORS

--- chatbotUphfApi/main.py
+++ chatbotUphfApi/main.py
@@ -1,8 +1,23 @@
 # main.py
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from controllers.historicController import router as historic_router
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:4200",  # Your Angular app's URL
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 # Enregistrer le module de routage auprès de l'instance FastAPI
 app.include_router(historic_router, prefix="/api/v1", tags=["historics"])
+  
