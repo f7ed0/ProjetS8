32764ab8385e399ebf72a16c97fd39d005cb5089
ahmedo <ahmed__daoud@outlook.fr>
2024-06-05 02:00:58 +0200

    Mise à jour des routes et des fichiers css

--- projetS8/src/app/app.component.ts
+++ projetS8/src/app/app.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, HttpClientModule,CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  
 })
 export class AppComponent {
   title = 'projetS8';
--- projetS8/src/app/app.routes.ts
+++ projetS8/src/app/app.routes.ts
@@ -1,6 +1,15 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 export const routes: Routes = [
-    {path : "home", component: MainComponent}
+    {path : "home", component: MainComponent},
+    {path : "chat/:chat_id", component: MainComponent}
 ];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule { }
