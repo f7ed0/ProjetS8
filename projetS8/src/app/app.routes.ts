62654b7efe966bb2403b189fc83c5f6272eda41a
VerselT <Verseltristan@gmail.com>
2024-06-05 11:31:43 +0200

    routes

--- projetS8/src/app/app.routes.ts
+++ projetS8/src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
+import { SuggComponent } from './sugg/sugg.component';
+import { authGuard } from './auth.guard';
 
 export const routes: Routes = [
-    {path : "home", component: MainComponent}
+    {path : "home", component: MainComponent},
+    {path: 'feedback', component: SuggComponent, canActivate: [authGuard] }
 ];
