1a479e7db050340922fb0d98db546035d00fe492
VerselT <Verseltristan@gmail.com>
2024-06-07 12:16:36 +0200

    main responsive

--- projetS8/src/app/main/main.component.ts
+++ projetS8/src/app/main/main.component.ts
@@ -1,26 +1,27 @@
-import {Component, ViewChild, AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef  } from '@angular/core';
-import {MatButtonModule} from '@angular/material/button';
-import {MatSidenavModule, MatDrawer} from '@angular/material/sidenav';
-import {MatIconModule} from '@angular/material/icon';
-import {MatTooltipModule} from '@angular/material/tooltip';
-import {MessagesComponent} from '../messages/messages.component';
+import { Component, ViewChild, AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule, MatDrawer } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MessagesComponent } from '../messages/messages.component';
 import { ConvsComponent } from '../convs/convs.component';
 import { NewconvComponent } from '../newconv/newconv.component';
 import { NewconvService } from '../newconv.service';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../header/header.component';
-
+import { DrawerService } from '../drawer.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-main',
   standalone: true,
   imports: [
-    MatButtonModule, 
-    MatSidenavModule, 
-    MatIconModule, 
-    MatTooltipModule, 
-    MessagesComponent, 
-    ConvsComponent, 
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatTooltipModule,
+    MessagesComponent,
+    ConvsComponent,
     NewconvComponent,
     CommonModule,
     HeaderComponent
@@ -33,11 +34,14 @@ export class MainComponent implements AfterViewInit {
   @ViewChild('drawer') drawer!: MatDrawer;
   isChevronLeft: boolean = true;
   showNewConv = true;
+  private drawerSubscription: Subscription | undefined;
+  screenWidth: number = 0;
 
   constructor(
     private newconvService: NewconvService,
-    private cdr: ChangeDetectorRef
-  ) {}
+    private cdr: ChangeDetectorRef,
+    private drawerService: DrawerService
+  ) { }
 
   ngAfterViewInit() {
     this.drawer.open();
@@ -45,6 +49,21 @@ export class MainComponent implements AfterViewInit {
       this.showNewConv = state;
       this.cdr.detectChanges();
     });
+
+    this.drawerSubscription = this.drawerService.toggleDrawer$.subscribe(() => {
+      this.toggleDrawer();
+    });
+    this.checkScreenWidth();
+    if(this.isMobile()) {
+      this.drawer.close();
+      this.toggleIcon();
+    }
+  }
+
+  ngOnDestroy() {
+    if (this.drawerSubscription) {
+      this.drawerSubscription.unsubscribe();
+    }
   }
 
   toggleDrawer() {
@@ -56,6 +75,11 @@ export class MainComponent implements AfterViewInit {
     this.isChevronLeft = !this.isChevronLeft;
   }
 
+  checkScreenWidth() {
+    this.screenWidth = window.innerWidth;
+  }
+
+  isMobile(): boolean {
+    return this.screenWidth < 768; 
+  }
 }
-
-
